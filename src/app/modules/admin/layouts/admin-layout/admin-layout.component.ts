f753276dbaaefe83d85c1d08e2f28144053e307d
Fernando Herrera <fernando.herrera85@gmail.com>
2024-08-07 12:57:53 -0400

    chore: Add apx-side-menu package and update dependencies

--- src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
+++ src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
@@ -2,16 +2,24 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideMenuComponent } from '../../../shared/components/side-menu/side-menu.component';
+import { ApxSideMenuComponent, TitleColor } from 'apx-side-menu';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SideMenuComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    SideMenuComponent,
+    ApxSideMenuComponent,
+  ],
   templateUrl: './admin-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class AdminLayoutComponent {
   isAuthenticated = signal(false);
+
+  TitleColor = TitleColor;
 
   onLogin() {
     this.isAuthenticated.set(true);
